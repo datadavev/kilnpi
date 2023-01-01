b74c9a4e0dbe3edae524db57dc79b47f1980009d
Dave <vieglais@kilnpi.beehivebeach.com>
2022-12-31 19:18:38 -0500

    added v amps

--- rh.py
+++ rh.py
@@ -1,3 +1,4 @@
+
 # SPDX-FileCopyrightText: 2021 ladyada for Adafruit Industries
 # SPDX-License-Identifier: MIT
 
@@ -10,8 +11,9 @@ import adafruit_dht
 import influxdb_client
 from influxdb_client import InfluxDBClient, Point, WritePrecision
 from influxdb_client.client.write_api import SYNCHRONOUS
+from ADCPi import ADCPi
 
-INTERVAL = 30
+INTERVAL = 10
 
 org = "dave@vieglais.com"
 client = influxdb_client.InfluxDBClient(
@@ -19,8 +21,14 @@ client = influxdb_client.InfluxDBClient(
   token=os.environ.get("INFLUXDB_TOKEN"),
   org=org
 )
-bucket="dht22_test"
+bucket="kiln"
 write_api = client.write_api(write_options=SYNCHRONOUS)
+
+adc_board = ADCPi(0x68, 0x69, 18)
+
+def get_reference_voltage():
+  return adc_board.read_voltage(4)
+
 
 def get_ipaddress():
   ipaddr = None
@@ -40,35 +48,75 @@ def vaporPressure(t, rh):
   svp = saturatedVaporPressure(t)
   return svp * rh/100
 
-class DHT22:
-  def __init__(self, pin, name:str):
+class BaseSensor:
+  def __init__(self, group: str, name:str):
+    self.group = group
+    self.name = name
+
+  def _preget_point(self):
+    return Point(self.group).tag("name", self.name)
+
+  def get_point(self):
+    point = (
+      self._preget_point()
+      .field("v", 0.0)
+    )
+    return point
+
+
+class DHT22(BaseSensor):
+  def __init__(self, pin, group:str, name:str):
+    super().__init__(group, name)
     self.device = adafruit_dht.DHT22(pin)
-    self.name=name
 
   def get_point(self):
     tc = self.device.temperature
     rh = self.device.humidity
     point = (
-      Point("test")
-      .tag("name", self.name)
+      self._preget_point()
       .field("T", tc)
       .field("RH", rh)
       .field("VP", vaporPressure(tc,rh))
     )
     return point
 
-def ip_point():
+class CurrentSensor(BaseSensor):
+  def __init__(self, group:str, name:str, channel:int):
+    super().__init__(group, name)
+    self.channel = channel
+
+  def get_point(self):
+    # https://www.amazon.com/dp/B0BB8YN9ZJ?psc=1&ref=ppx_yo2ov_dt_b_product_details
+    # 0 amp = vcc / 2
+    # 66 mV / A
+    v_ref = get_reference_voltage()
+    v = adc_board.read_voltage(self.channel)
+    v_0 = v_ref / 2.0
+    amps = (v_0 - v) / 0.066
+    return (
+      self._preget_point()
+      .field("v0", v_0)
+      .field("v", v)
+      .field("A", amps)
+    )
+
+def ip_point(group):
   ipaddr = get_ipaddress()
   return (
-    Point("test")
+    Point(group)
     .tag("name", "ipaddr")
     .field("ip", ipaddr)
   )
 
 sensors = []
-sensors.append(DHT22(board.D17, name="HT-1"))
-sensors.append(DHT22(board.D18, name="HT-2"))
-sensors.append(DHT22(board.D19, name="HT-3"))
+group = "kiln"
+sensors.append(DHT22(board.D17, group, name="HT-1"))
+sensors.append(DHT22(board.D27, group, name="HT-2"))
+sensors.append(DHT22(board.D22, group, name="HT-3"))
+sensors.append(DHT22(board.D18, group, name="HT-4"))
+sensors.append(CurrentSensor(group, "Fan-1", 1))
+sensors.append(CurrentSensor(group, "Fan-2", 2))
+sensors.append(CurrentSensor(group, "Fan-3", 3))
 while True:
   for sensor in sensors:
     try:
@@ -77,6 +125,6 @@ while True:
       write_api.write(bucket=bucket, org=org, record=point)
     except RuntimeError as e:
       print(e)
-  write_api.write(bucket=bucket, org=org, record=ip_point())
+  write_api.write(bucket=bucket, org=org, record=ip_point(group))
   time.sleep(INTERVAL)
 
